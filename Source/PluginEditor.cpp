3eb6daf402b2c0a4078fa55fabe12e9247e8a484
sjoesnds <centralizedboss@gmail.com>
2026-09-25 19:36:17 +0300

    0.51: bump visible plugin version

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -67,7 +67,7 @@ setSize(900,800);
 setResizable(true, true);
 setResizeLimits(980, 720, 1400, 1100);
 title.setText("MIDI FORGE",juce::dontSendNotification);
-versionLabel.setText("v0.50.0", juce::dontSendNotification);
+versionLabel.setText("v0.51.0", juce::dontSendNotification);
 versionLabel.setColour(juce::Label::textColourId, juce::Colours::white.withAlpha(0.55f));
 versionLabel.setFont(juce::Font(11.0f));
 addAndMakeVisible(versionLabel);
